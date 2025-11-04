8d2ec655e978e116abcf091f9b85664602f40c5e
gragra02 <gragra02@gmail.com>
2025-11-04 17:56:34 -0300

    Primeiro commit e iniciando o projeto

---
+++ src/main/java/projetopessoal/projetotodo/ProjetoToDoApplication.java
@@ -0,0 +1,13 @@
+package projetopessoal.projetotodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoToDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projetopessoal/projetotodo/ProjetoToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package projetopessoal.projetotodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
