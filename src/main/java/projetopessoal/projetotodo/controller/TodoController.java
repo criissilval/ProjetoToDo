236af7920020092a87062b7a7d91f2a408cb9ada
Cristian <Cris.laurentino28@gmail.com>
2025-11-04 18:49:44 -0300

    Arquivo TodoController atualizado

---
+++ src/main/java/projetopessoal/projetotodo/controller/TodoController.java
@@ -0,0 +1,33 @@
+package projetopessoal.projetotodo.controller;
+
+import org.springframework.web.bind.annotation.*;
+import projetopessoal.projetotodo.entity.Todo;
+import projetopessoal.projetotodo.service.TodoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+    @GetMapping
+    List<Todo> list(){
+        return  todoService.list();
+    }
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+}
